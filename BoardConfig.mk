74bfde31ff55f5c0e054fbd22619e094e498b6f4
AndropaX <kpax.mar@gmail.com>
2016-12-17 15:38:37 +0100

    switch selinux to enforcing
    Change-Id: I222303ef2fbe4b75e8449e6419ba6c905c6b8f2e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,7 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom boot_cpus=0-5
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 msm_poweroff.download_mode=0
 #BOARD_KERNEL_CMDLINE += synaptics_dsx.startup_fw_update=1
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += cpu_max_a53=1632000 cpu_max_a57=2016000
 
 #BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
