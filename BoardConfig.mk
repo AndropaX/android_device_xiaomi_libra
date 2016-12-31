495d3ad750aafcf8375d1bafe3525f52a1969224
AndropaX <kpax.mar@gmail.com>
2016-12-31 11:42:15 +0100

    Revert "use wlan module"
    This reverts commit 4049880c50528b490e8342d79b59a01f0818e799.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,8 +122,6 @@ TARGET_USES_QCOM_WCNSS_QMI       := true
 TARGET_USES_WCNSS_MAC_ADDR_REV   := true
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
-WIFI_DRIVER_MODULE_NAME          := "wlan"
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 
 BOARD_USES_SECURE_SERVICES := true
