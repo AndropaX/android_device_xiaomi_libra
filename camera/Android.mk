110ad80780a20b9ec94d6543198902234a642e23
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-01-31 08:12:31 +0200

    Update and clean up
    GPS from Mi Max.
    
    Change-Id: Ia32e8be876a9e752ec2dc1e92facd0ce493de5f1

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,24 +11,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := \
-    system/media/camera/include
-
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libgui libutils
+    libhardware liblog libutils libgui
+
+LOCAL_C_INCLUDES += \
+    system/media/camera/include
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_32_BIT_ONLY := true
+LOCAL_32_BIT_ONLY := $(BOARD_QTI_CAMERA_32BIT_ONLY)
+
 include $(BUILD_SHARED_LIBRARY)
--- rootdir/bin/init.qcom.sh
+++ rootdir/bin/init.qcom.sh
@@ -1,5 +1,15 @@
 #!/system/bin/sh
 
+start_copying_prebuilt_qcril_db()
+{
+    if [ -f /system/vendor/qcril.db -a ! -f /data/misc/radio/qcril.db ]; then
+        cp /system/vendor/qcril.db /data/misc/radio/qcril.db
+        chown -h radio.radio /data/misc/radio/qcril.db
+    fi
+}
+
+start_copying_prebuilt_qcril_db
+
 #
 # Make modem config folder and copy firmware config to that folder
 #
