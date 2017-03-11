ea39be5b9330f6a91e225dc09ea4d6da6bd175d2
AndropaX <kpax.mar@gmail.com>
2017-03-11 10:12:28 +0100

    use caf telephony config
    Change-Id: Idb87a3a471d10cab8d943e8253ba2494b9c3cc9d

--- lineage.mk
+++ lineage.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
