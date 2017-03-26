c26b9f66907c0798d45a151dd1f27a00bb771bc9
AndropaX <kpax.mar@gmail.com>
2017-03-26 10:28:54 +0200

    Use miui stable fingerprint
    Avoid cts profile missmatch error by safetynet,

--- lineage.mk
+++ lineage.mk
@@ -26,8 +26,8 @@ TARGET_VENDOR := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="libra" \
-    BUILD_FINGERPRINT="Xiaomi/libra/libra:7.0/NRD90M/7.2.16:user/release-keys" \
-    PRIVATE_BUILD_DESC="libra-user 7.0 NRD90M 7.2.16 release-keys"
+    BUILD_FINGERPRINT="Xiaomi/libra/libra:5.1.1/LMY47V/V8.1.2.0.LXKCNDI:user/release-keys" \
+    PRIVATE_BUILD_DESC="libra-user 5.1.1 LMY47V V8.1.2.0.LXKCNDI release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
     
