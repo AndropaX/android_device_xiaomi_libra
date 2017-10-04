20d0b8072b9f46b59c715bd63ec3161efc7b4601
AndropaX <kpax.mar@gmail.com>
2017-10-04 17:22:24 +0200

    revert consumerir changes
    Change-Id: I923d18e83cd8afa0652dde6d5ba45e27c1caac2e

--- device.mk
+++ device.mk
@@ -212,7 +212,8 @@ PRODUCT_COPY_FILES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.libra
+    android.hardware.ir@1.0-impl \
+    consumerir.msm8992
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
